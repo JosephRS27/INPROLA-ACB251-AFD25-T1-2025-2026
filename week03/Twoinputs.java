c376bebfe299a9df900e69770ac92adccea4db71
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-07 10:48:39 +0800

    Initial commit

---
+++ week03/Twoinputs.java
@@ -0,0 +1,21 @@
+public class Twoinputs {
+    public static void main (Stings args[])
+         Scanner input = new Scanner(System.in);
+         
+         // Asking for two integers inputs 
+        System.out.print("Enter the first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = input.nextInt();
+        
+        // Perform and display operations
+        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("Difference: " + (num1 - num2));
+        System.out.println("Product: " + (num1 * num2));
+        System.out.println("Quotient: " + (num1 / num2));
+
+        input.close();
+    }
+        
+}
