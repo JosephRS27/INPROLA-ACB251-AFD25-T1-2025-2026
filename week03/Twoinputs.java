6173d3f89c5eef6baa2220c2a2b0468d09d00371
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-07 10:55:45 +0800

    Intial commit

--- week03/Twoinputs.java
+++ week03/Twoinputs.java
@@ -14,8 +14,7 @@ public class Twoinputs {
         System.out.println("Difference: " + (num1 - num2));
         System.out.println("Product: " + (num1 * num2));
         System.out.println("Quotient: " + (num1 / num2));
-
-        input.close();
+        
     }
         
 }
