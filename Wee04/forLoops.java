952251a7a2308dc6c175e6a71335f6517c69a04d
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-11 02:30:02 +0000

    Commit

---
+++ Wee04/forLoops.java
@@ -0,0 +1,10 @@
+public class forLoops {
+  public static void main(String[] args) {
+
+    System.out.println("Enter two integers individually:");
+
+    // First input
+    int firstInteger  = input.nextInt();
+
+    // Second input
+    int secondInteger = input.nextInt();
