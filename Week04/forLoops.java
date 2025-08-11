af814cef075b7c0c2d09b743875d4810b35b934d
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-11 03:43:51 +0000

    commit

--- Week04/forLoops.java
+++ Week04/forLoops.java
@@ -3,13 +3,12 @@ public class forLoops {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Enter starting number:");
-    System.out.println("Enter ending number:");
-
     // First num
+    System.out.println("Enter starting number:");
     int start  = input.nextInt();
 
     // Second num
+    System.out.println("Enter ending number:");
     int end = input.nextInt();
 
     for (int i = start; i <= end; i++){
