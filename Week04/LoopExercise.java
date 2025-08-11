9270d9eb36c4e155768eea31f421c4b6d5777d58
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-11 03:19:41 +0000

    Updated

--- Week04/LoopExercise.java
+++ Week04/LoopExercise.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class LoopExercise {
   public static void main(String[] args) {
-  Scanner userName = new Scanner(System.in);
+  Scanner input = new Scanner(System.in);
 
     System.out.println("Enter Username: ");
 
     // First input
-    String userName  = userName.nextLine();
+    String userName  = input.nextLine();
 
     // Output input by user
     System.out.println("Username is" + userName);
