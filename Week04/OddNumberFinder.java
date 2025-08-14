7e2b1cc7eb92dfe31312f89c1191ab7381d9065c
Joseph Serrano <jrserrano@student.apc.edu.ph>
2025-08-14 02:09:16 +0000

    NewMethod

--- Week04/OddNumberFinder.java
+++ Week04/OddNumberFinder.java
@@ -24,4 +24,17 @@ public class OddNumberFinder {
             }
         }
     }
-}
+      public void findAndDisplayOdds2(int start, int end) {
+        System.out.println("\nOdd numbers in the range " + start + " to " + end + " are:");
+ 
+        // Use a for loop to iterate from the start point to the end point
+        while (int i = start; i <= end; i++) {
+            // Check if the current number (i) is even.
+            // A number is even if the remainder when divided by 2 is 0.
+            if (i % 2 != 0) {
+                // If the number is even, print it to the console
+                System.out.println(i);
+            }
+        }
+    }
+}     
